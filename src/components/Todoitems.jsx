2d03163f354fc2151a226b005260b9ee263283af
Aditi <additii21@gmail.com>
2025-08-27 11:29:29 +0530

    Tried fixing CSS file import in Todoitems again

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -1,4 +1,4 @@
-import './CSS/Todoitems.css'
+import './CSS/TodoItems.css'
 import tick from './Assets/tick.png';
 import not_tick from './Assets/not_tick.png' ;
 import cross from "./Assets/cross.png";
